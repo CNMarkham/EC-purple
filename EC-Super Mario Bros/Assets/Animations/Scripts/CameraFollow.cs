3edab923eebab173960de5a3b32b7a943dbf670f
Roosham <markhamonca@codeninjas.com>
2024-02-17 15:01:13 -0500

    the
    the

---
+++ EC-Super Mario Bros/Assets/Animations/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public Transform target;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target.position.x > transform.position.x)
+        {
+            transform.position = new Vector3(target.position.x, transform.position.y, transform.position.z);
+        }
+        if (target.position.y < transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y, transform.position.z);
+        }
+        if (target.position.y > transform.position.y)
+        {
+            transform.position = new Vector3(transform.position.x, target.position.y, transform.position.z);
+        }
+    }
+}
