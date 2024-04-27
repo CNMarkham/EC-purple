618fc463ce6f61f13f7fbd41c5d2bcdc401d67b7
Madelyn <markhamonca@codeninjas.com>
2024-04-27 14:54:29 -0400

    me after erhanbadkid
    the

--- EC-Tetris/Assets/Scripts/Tetromino.cs
+++ EC-Tetris/Assets/Scripts/Tetromino.cs
@@ -2,40 +2,122 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 public class Tetromino : MonoBehaviour
 {
-    private float previousTime;
-    public float fallTime = 0.8f;
-    private float tempTime = 0;
-    // Update is called once per frame
+                              private     float        previousTime;
+             public               float                          fallTime                        = 0.8f;
+                    private              float                       tempTime =           0;
+                         public                       int                                                  width =                                                     10;
+             public         static                  int          height                                                                               =20;
+                                     //            Update          is              called        once     per             frame
+
     void Update()
     {
+
         if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
             transform.position += Vector3.left;
+
+            if (!ValidMove())
+            {
+                transform.position += Vector3.right;
+            }
         }
         if (Input.GetKeyDown(KeyCode.RightArrow))
         {
             transform.position += Vector3.right;
+            if (!ValidMove())
+            {
+                transform.position += Vector3.left;
+            }
         }
-        // Comparing Time.time - previous time to fallTime
-        // Time.time the current time
+        //          Comparing    Time . time    -               previous   time   to            fallTime
+        //      Time    . time       the                  current          time
         // previousTime is the last time been updated
-        if (Time.time - previousTime > fallTime)
+        tempTime = fallTime;
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            tempTime = tempTime / 10;
+        }
+        if (Time.time - previousTime > tempTime)
         {
             transform.position += Vector3.down;
-            // when the previousTime gets updated
+            //            when         the                                  previousTime           gets            updated
             previousTime = Time.time;
-            //fallTime = tempTime;
+            if (!ValidMove())
+            {
+                transform.position += Vector3.up;
+            }
+
+
 
         }
-        if (Input.GetKeyDown(KeyCode.DownArrow))
+
+
+    }
+    public bool ValidMove()
+    {
+        foreach (Transform child in transform)
         {
-            //tempTime = fallTime;
-            //tempTime = tempTime / 10;
-            
-            
-        }
+            int x = Mathf.RoundToInt(child.transform.position.x);
+            int y = Mathf.RoundToInt(child.transform.position.y);
+            if (x < 0)
+            {
+                return false;
 
+            }
+            if (y < 0)
+            {
+
+                return false;
+            }
+            if (y >= height)
+            {
+                return false;
+
+            }
+            if (x >= width)
+            {
+                return false;
+
+            }
+        }
+        return true;
     }
 }
